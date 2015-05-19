fdcec833875217c84b6943208f83b3a3456e6abe
Peter Saxton <peterhsaxton@gmail.com>
2015-05-19 12:40:00 +0100

    pages

--- app/controllers/properties.rb
+++ app/controllers/properties.rb
@@ -6,5 +6,29 @@ module Maenporth
       render :index
     end
 
+    get '/for-rent' do
+      render :'all-for-rent'
+    end
+
+    get '/for-sale' do
+      render :'all-for-sale'
+    end
+
+    get '/1/edit' do
+      render :edit
+    end
+
+    get '/1/for-rent' do
+      render :'for-rent'
+    end
+
+    get '/1/for-sale' do
+      render :'for-sale'
+    end
+
+    get '/1/enquire' do
+      render :enquire
+    end
+
   end
 end
