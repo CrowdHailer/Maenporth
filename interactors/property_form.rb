7dc43f7667860d659da0107b9ac59583f3eb185a
Peter Saxton <peterhsaxton@gmail.com>
2015-06-30 10:00:05 +0100

    add checklist types

--- interactors/property_form.rb
+++ interactors/property_form.rb
@@ -1,4 +1,9 @@
 class PropertyForm < Vulcanize::Form
   attribute :sale_description, Description
   attribute :sale_price, SalePrice
+  attribute :estate_agent_link, Link
+  attribute :property_type, PropertyType
+  attribute :bedrooms, Bedrooms
+  attribute :bathrooms, Bathrooms
+  attribute :view_type, ViewType
 end
--- test/forms/property_form_test.rb
+++ test/forms/property_form_test.rb
@@ -14,4 +14,34 @@ class PropertyFormTest < Minitest::Test
     form = PropertyForm.new :sale_price => raw
     assert_equal sale_price, form.sale_price
   end
+
+  def test_obtains_estate_agent_link
+    link = 'link.example.com'
+    form = PropertyForm.new :estate_agent_link => link
+    assert_equal link, form.estate_agent_link
+  end
+
+  def test_obtains_property_type
+    property_type = 'Flat'
+    form = PropertyForm.new :property_type => property_type
+    assert_equal property_type, form.property_type
+  end
+
+  def test_obtains_bedrooms
+    bedrooms = '5+'
+    form = PropertyForm.new :bedrooms => bedrooms
+    assert_equal bedrooms, form.bedrooms
+  end
+
+  def test_obtains_bathrooms
+    bathrooms = '3+'
+    form = PropertyForm.new :bathrooms => bathrooms
+    assert_equal bathrooms, form.bathrooms
+  end
+
+  def test_obtains_view_type
+    view_type = 'Sea View'
+    form = PropertyForm.new :view_type => view_type
+    assert_equal view_type, form.view_type
+  end
 end
