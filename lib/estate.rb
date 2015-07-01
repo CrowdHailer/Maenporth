35c09630a53192a6dac110acbd11881e9d4efb1d
Peter Saxton <peterhsaxton@gmail.com>
2015-07-01 10:52:39 +0100

    rental and sale methods on repository

--- lib/estate.rb
+++ lib/estate.rb
@@ -1,5 +1,7 @@
 class Estate < Errol::Repository
   class Inquiry < Errol::Inquiry
+    default :for_sale, true
+    default :for_rent, true
     default :page, 1
     default :page_size, 15
   end
@@ -11,6 +13,14 @@ class Estate < Errol::Repository
 
     def inquiry(requirements)
       Inquiry.new requirements
+    end
+
+    def for_rent
+      all :for_sale => false
+    end
+
+    def for_sale
+      all :for_rent => false
     end
 
     def dispatch(record)
@@ -26,7 +36,15 @@ class Estate < Errol::Repository
 
   def dataset
     # TODO errol allow no dataset method
-    raw_dataset
-    # raw_dataset.order :title
+    tmp = raw_dataset
+
+    unless inquiry.for_sale
+      tmp = tmp.exclude(:for_sale => true)
+    end
+
+    unless inquiry.for_rent
+      tmp = tmp.exclude(:for_rent => true)
+    end
+    tmp
   end
 end
