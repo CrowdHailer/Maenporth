e6f0f3a9b9fca99a38669d75fb032d8d22decc7e
Peter Saxton <peterhsaxton@gmail.com>
2015-07-02 12:18:08 +0100

    configure to use S3

--- lib/image_uploader.rb
+++ lib/image_uploader.rb
@@ -1,17 +1,3 @@
-CarrierWave.configure do |config|
-
-  # config.storage    = :aws
-  # config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
-  # # config.aws_acl    = :public_read
-  # # config.asset_host = 'http://example.com'
-  # config.aws_authenticated_url_expiration = 60 * 60
-  #
-  # config.aws_credentials = {
-  #   access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-  #   secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
-  # }
-  config.root 'public'
-end
 require 'carrierwave/processing/mini_magick'
 
 class ImageUploader < CarrierWave::Uploader::Base
