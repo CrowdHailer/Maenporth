2a8f63c516def01860bfb465cf6ede6b011c6a8c
Peter Saxton <peterhsaxton@gmail.com>
2015-07-17 10:38:08 +0100

    configure mail sending with mandrill

--- app/mailers/property.rb
+++ app/mailers/property.rb
@@ -1,3 +1,4 @@
+# TODO untested
 class PropertyMailer
   def initialize(property)
     @property = property
@@ -6,8 +7,9 @@ class PropertyMailer
   attr_reader :property
 
   def enquiry(data)
-    to = [property.enquiry_email, ENV['ADMIN_EMAIL']]
-    from = 'info@etc.com'
+    to = property.enquiry_email
+    cc = ENV['ADMIN_EMAIL']
+    from = 'no-reply@maenporthestate.com'
 
     prospect_name = data['name']
     prospect_email_address = data['email']
@@ -17,6 +19,21 @@ class PropertyMailer
 
     file_path = File.expand_path('../property/enquiry.txt.erb', __FILE__)
     template = ERB.new File.read(file_path)
-    ap template.result binding
+
+    text_body = template.result binding
+
+    mail = Mail.new
+    mail.from from
+    mail.to to
+    mail.cc cc
+
+    # text_part = Mail::Part.new do
+    #   body text_body
+    # end
+    # mail.text_part = text_part
+    mail.body = text_body
+
+    mail.deliver
+
   end
 end
