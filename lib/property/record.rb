95defbfdbcb7cce543abf032b962f9c32274357f
CrowdHailer <peterhsaxton@gmail.com>
2015-08-13 16:19:50 +0100

    fix html method on string

--- lib/property/record.rb
+++ lib/property/record.rb
@@ -10,6 +10,7 @@ module Property
 
     serialize_attributes [Description.method(:dump), Description.method(:new)], :sale_description
     serialize_attributes [Description.method(:dump), Description.method(:new)], :rent_description
+    serialize_attributes [Description.method(:dump), Description.method(:new)], :review
     serialize_attributes [SalePrice.method(:dump), SalePrice.method(:new)], :sale_price
     serialize_attributes [->(x){x}, Link.method(:new)], :estate_agent_link
     serialize_attributes [->(x){x}, Link.method(:new)], :enquiry_link
